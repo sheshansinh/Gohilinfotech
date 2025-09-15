97b547e778e1d79a1b3f3b268f0431bfdd9866a6
ayush <147644983+sheshansinh@users.noreply.github.com>
2025-09-15 18:34:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    // Disable ESLint during build
+    eslint: {
+      // or set to 'warning' if you want to see warnings but not fail the build
+      failOnWarning: false,
+      failOnError: false,
+    },
+  },
+});
